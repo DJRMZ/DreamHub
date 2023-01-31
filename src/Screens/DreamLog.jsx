8dba7bd214409e698567834bbfdeb847d00c9118
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-30 19:35:26 -0800

    centered components, still working on padding or margin top and spacing overall

--- src/Screens/DreamLog.jsx
+++ src/Screens/DreamLog.jsx
@@ -15,31 +15,30 @@ const configuration = new Configuration({
 });
 
 const AIDreamGen = () => {
-  const [dream, setDream] = useState('');
-  const [dreamImg, setDreamImg] = useState('');
+  const [dream, setDream] = useState("");
+  const [dreamImg, setDreamImg] = useState("");
   const [loading, setLoading] = useState(false);
   const [supabase, setSupabase] = useState(null);
-  
+
   const openai = new OpenAIApi(configuration);
 
   useEffect(() => {
-    const supabase = (async () => {
-      const token = await getToken({ template: 'supabase' });
+    const supabase = async () => {
+      const token = await getToken({ template: "supabase" });
       const supabase = await supabaseClient(token);
       return supabase;
-    });
+    };
 
     setSupabase(supabase);
   }, []);
 
   const { getToken } = useAuth();
 
-  
   // console.log('OPENAI', openai);
 
   const handleDreamChange = (input) => {
     setDream(input);
-    console.log('DREAM', dream);
+    console.log("DREAM", dream);
   };
 
   const handleSubmitDream = async () => {
@@ -49,15 +48,15 @@ const AIDreamGen = () => {
       n: 1, // number of images to generate
       size: "1024x1024",
     };
-    console.log('DREAM OBJECT', dreamObject);
+    console.log("DREAM OBJECT", dreamObject);
     try {
       const response = await openai.createImage(dreamObject);
 
       // console.log('RESPONSE BODY', response);
-      console.log('RESPONSE URL', response.data.data[0].url);
+      console.log("RESPONSE URL", response.data.data[0].url);
       let image_url = response.data.data[0].url;
       setDreamImg(image_url);
-      console.log('IMAGE URL', image_url);
+      console.log("IMAGE URL", image_url);
     } catch (error) {
       console.log(error);
     }
@@ -67,12 +66,12 @@ const AIDreamGen = () => {
   return (
     <>
       <LinearGradient
-        colors={['#a07efe', '#3b5998']}
+        colors={["#a07efe", "#3b5998"]}
         style={{
-          position: 'absolute',
+          position: "absolute",
           inset: 0,
-          height: '100%',
-          width: '100%',
+          height: "100%",
+          width: "100%",
         }}
       />
       <SleepNotes />
