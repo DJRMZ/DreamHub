153959a3ce1a812e52e853da0db5e4175301ebe1
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-30 18:40:10 -0800

    working on pages/screens

--- src/Screens/AccountScreen.jsx
+++ src/Screens/AccountScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import SignOutButton from "../Auth/SignOut";
 
@@ -7,6 +7,9 @@ const AccountScreen = () => {
     <View>
       <Text>Account</Text>
       <Text>How to use our app:</Text>
+      <Text>Use the prompt window to describe your dream. Here are a couple examples:</Text>
+      <Text>A pink bunny at the beach with a giraffe drinking lemonade</Text>
+      {/* <Image source={./}></Image> */}
       <Text>Tips and Tricks</Text>
       <SignOutButton />
     </View>
