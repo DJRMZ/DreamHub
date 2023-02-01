e8a22f83feb433b9103272057371bf384b3115a8
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-31 17:44:57 -0800

    Update: Home and Account pages layouts set up, rough draft for content placement and future styling

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -3,65 +3,72 @@ import DreamCalendar from "../Components/DreamCalendar";
 import Camera from "./Camera";
 import { Button, Card, Modal, Text } from "@ui-kitten/components";
 import { StyleSheet } from "react-native";
-import { ScrollView } from "native-base";
+import { ScrollView } from "react-native-gesture-handler";
 
 import * as React from "react";
-import { Dimensions, View } from "react-native";
+import { Dimensions, Image, View } from "react-native";
 // import Carousel from "react-native-reanimated-carousel";
 
 const HomeScreen = () => {
   const width = Dimensions.get("window").width;
+  const exampleImage = require("../../assets/example.png");
 
   return (
-    <View style={styles.backdrop}>
-      <View style={styles.container}>
-        <Text>Home</Text>
-        <Text>How to use this app:</Text>
-        <Text>
-          The Dream UI button on the home screen or the Dream UI tab at the
-          bottom of the screen will take you to the screen to input some text to
-          describe your dream. There are different prompts you can use,
-          including time, environment, nouns and verbs. Be as descriptive as you
-          like and see what image you can create!
-        </Text>
-        <Card>
-          <Button
-            onPress={() => {
-              <DreamGen />;
-            }}
-          >
-            Create an Image
-          </Button>
-        </Card>
-        <Card>
-          <Button
-            onPress={() => {
-              <Notes />;
-            }}
-          >
-            Journal/Notes
-          </Button>
-        </Card>
-        <Card>
-          <Button
-            onPress={() => {
-              <Camera />;
-            }}
-          >
-            Take a picture
-          </Button>
-        </Card>
-        <Card>
-          <Button
-            onPress={() => {
-              <DreamCal />;
-            }}
-          >
-            Calendar
-          </Button>
-        </Card>
-        <View style={{ flex: 1 }}>
-          {/* <Carousel
+    <ScrollView>
+      <View style={styles.backdrop}>
+        <View style={styles.container}>
+          <Text>
+            Welcome to DreamHub! Journaling for creatives in tech with a twist.{" "}
+          </Text>
+          <Text>How to use this app:</Text>
+          <Text>
+            The Dream UI button on the home screen or the Dream UI tab at the
+            bottom of the screen will take you to the screen to input some text
+            to describe your dream. There are different prompts you can use,
+            including time, environment, nouns and verbs. Be as descriptive as
+            you like and see what image you can create!
+          </Text>
+          <Card style={styles.card}>
+            <Button
+              onPress={() => {
+                <DreamGen />;
+              }}
+            >
+              Create an Image
+            </Button>
+          </Card>
+          <Card style={styles.card}>
+            <Button
+              onPress={() => {
+                <Notes />;
+              }}
+            >
+              Journal/Notes
+            </Button>
+          </Card>
+          <Card style={styles.card}>
+            <Button
+              onPress={() => {
+                <Camera />;
+              }}
+            >
+              Take a picture
+            </Button>
+          </Card>
+          <Card style={styles.card}>
+            <Button
+              onPress={() => {
+                <DreamCal />;
+              }}
+            >
+              Calendar
+            </Button>
+          </Card>
+          <View style={{ flex: 1 }}>
+            <Card>
+              <Image style={styles.image} source={exampleImage} />
+            </Card>
+            {/* <Carousel
             loop
             width={width}
             height={width / 2}
@@ -71,21 +78,22 @@ const HomeScreen = () => {
             onSnapToItem={(index) => console.log("current index:", index)}
             renderItem={({ index }) => (
               <View
-                style={{
-                  flex: 1,
-                  borderWidth: 1,
-                  justifyContent: "center",
-                }}
+              style={{
+                flex: 1,
+                borderWidth: 1,
+                justifyContent: "center",
+              }}
               >
-                <Text style={{ textAlign: "center", fontSize: 30 }}>
-                  {index}
-                </Text>
+              <Text style={{ textAlign: "center", fontSize: 30 }}>
+              {index}
+              </Text>
               </View>
-            )}
-          /> */}
+              )}
+            /> */}
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -98,9 +106,27 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+    // paddingTop: 10,
+    // paddingBottom: 10,
   },
   backdrop: {
     backgroundColor: "rgba(0, 0, 0, 0.7)",
     minHeight: "100%",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  image: {
+    maxWidth: 100,
+    maxHeight: 100,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  card: {
+    paddingTop: 10,
+    paddingBottom: 10,
   },
 });
