a30cd01106cb9329e65ce88495e564522d11cc26
Julian Barker <julian.c.barker@gmail.com>
2023-02-01 09:17:48 -0800

    feat: working db write

--- src/Screens/DreamLogger.jsx
+++ src/Screens/DreamLogger.jsx
@@ -1,15 +1,13 @@
-import { useState, useEffect } from "react";
-import { ScrollView, Text, Button, View, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
-// import SleepNotes from "../Components/SleepNotes";
 import DreamGenerator from "../Components/AIDreamGen";
 
 const DreamLogger = () => {
+
   return (
     <>
       <LinearGradient
-        colors={['#181d37','#535c75']}
+        colors={['#181d37', '#535c75']}
         style={{
           position: 'absolute',
           inset: 0,
