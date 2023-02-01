1ceeecae46fb79cd97238c3329bc8f4b6618a4b8
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-31 16:05:19 -0800

    Update: working on home screen trying to add carousel to show example images maybe?

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -6,7 +6,13 @@ import { Button, Card, Modal, Text } from "@ui-kitten/components";
 import { StyleSheet } from "react-native";
 import { ScrollView } from "native-base";
 
+import * as React from "react";
+import { Dimensions, View } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
+
 const HomeScreen = () => {
+  const width = Dimensions.get("window").width;
+
   return (
     <View style={styles.backdrop}>
       <View style={styles.container}>
@@ -55,6 +61,30 @@ const HomeScreen = () => {
             Calendar
           </Button>
         </Card>
+        <View style={{ flex: 1 }}>
+          <Carousel
+            loop
+            width={width}
+            height={width / 2}
+            autoPlay={true}
+            data={[...new Array(6).keys()]}
+            scrollAnimationDuration={1000}
+            onSnapToItem={(index) => console.log("current index:", index)}
+            renderItem={({ index }) => (
+              <View
+                style={{
+                  flex: 1,
+                  borderWidth: 1,
+                  justifyContent: "center",
+                }}
+              >
+                <Text style={{ textAlign: "center", fontSize: 30 }}>
+                  {index}
+                </Text>
+              </View>
+            )}
+          />
+        </View>
       </View>
     </View>
   );
